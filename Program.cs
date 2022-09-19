d2d96b91802eee559a0adf35fcfca67e53b3bd69
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-19 19:48:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+int[] massiv = new int[numberA];
+
+massiv1(massiv);
+Print(massiv);
+
+
+void massiv1(int[] massiv)
+{
+for(int i = 0; i < massiv.Length; i++)
+{
+	massiv[i] = new Random().Next(100,1000);
+}
+}
+
+
+void Print(int[] massiv)
+{
+	for(int i = 0; i < massiv.Length; i++)
+	{
+    
+		if(massiv[i]%2 == 0)
+		{
+		
+	    	Console.Write(massiv[i] + ", ");
+	    	
+		}
+	
+
+	}
+}
